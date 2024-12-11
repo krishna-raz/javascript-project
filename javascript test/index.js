79771b62baeced356c4b301e18906ca1a2be91e9
Krishna Raj <78550375+krishna-raz@users.noreply.github.com>
2024-12-11 18:29:45 +0530

    add clock

---
+++ javascript test/index.js
@@ -0,0 +1,19 @@
+// const date2=new Date();
+// console.log(date2);//this is the current date of international standard
+// console.log(date2.toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }));// this is indian standard date
+
+   
+time =new Date().toLocaleTimeString();
+
+console.log(time);
+// function hello(){
+//     console.log("hello world");
+// }
+// // setTimeout(hello,3000);
+// interval=setInterval(hello,3000);
+
+// setTimeout(()=>{
+//     clearInterval(interval);
+// },8000);
+
+
